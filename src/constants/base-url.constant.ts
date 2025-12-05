82cf9030189d4ab4a73c178db614af71fa0e5cb0
Joshua Vincent Padilla <joshuapadilla706@gmail.com>
2025-12-05 15:59:23 +0800

    for demo

--- src/constants/base-url.constant.ts
+++ src/constants/base-url.constant.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://192.168.101.2:3000/";
+// export const BASE_URL = "http://192.168.101.2:3000/";
 // export const BASE_URL = "http://172.20.10.2:3000/";
-// export const BASE_URL = "http://3.104.109.81:3000/";
+export const BASE_URL = "http://3.104.109.81:3000/";
